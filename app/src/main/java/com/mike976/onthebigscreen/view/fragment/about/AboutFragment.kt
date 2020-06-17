68606d9fc32ca78103dc9c93a8178b65105a43f6
Your Name <you@example.com>
2020-06-17 12:40:47 +0100

    completed about page

--- app/src/main/java/com/mike976/onthebigscreen/view/fragment/about/AboutFragment.kt
+++ app/src/main/java/com/mike976/onthebigscreen/view/fragment/about/AboutFragment.kt
@@ -1,22 +1,18 @@
 package com.mike976.onthebigscreen.view.fragment.about
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.onthebigscreen.R
-import com.mike976.onthebigscreen.viewmodel.AboutViewModel
+import com.mike976.onthebigscreen.util.getProgressDrawable
+import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
-
-    companion object {
-        fun newInstance() =
-            AboutFragment()
-    }
-
-    private lateinit var viewModel: AboutViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +23,54 @@ class AboutFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(AboutViewModel::class.java)
+        loadImages()
 
     }
+
+    private fun loadImages() {
+
+        activity?.let {
+            val progressDrawable = getProgressDrawable(it)
+            appIcon.setImageResource(R.drawable.about)
+
+            tmDbIcon.setImageResource(R.drawable.tmdb_logo)
+            gitHubImage.setImageResource(R.drawable.github)
+            gitHubImage.setOnClickListener {
+                it.context.browse("https://github.com/mike976")
+            }
+
+            emailImage.setImageResource(R.drawable.email)
+            emailImage.setOnClickListener {
+                it.context.email("mikebullock976@gmail.com")
+            }
+
+            linkedInImage.setImageResource(R.drawable.linked_in)
+            linkedInImage.setOnClickListener {
+                it.context.browse("https://uk.linkedin.com/in/michaelbullock976")
+            }
+
+            shareImage.setImageResource(R.drawable.share)
+            shareImage.setOnClickListener {
+                it.context.share("Hey there, Check out the 'On the Big Screen app! (Android)'")
+            }
+        }
+
+    }
+
+    fun Context.share(text: String) =
+        this.startActivity(Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, text)
+            type = "text/plain"
+        })
+
+    fun Context.browse(text: String) =
+        this.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(text)))
+
+    fun Context.email(emailAddr: String) =
+        this.startActivity(Intent().apply {
+            action = Intent.ACTION_SENDTO
+            data = Uri.parse("mailto:$emailAddr")
+        })
 
 }
--- app/src/main/java/com/mike976/onthebigscreen/viewmodel/AboutViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.mike976.onthebigscreen.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class AboutViewModel : ViewModel() {
-}
